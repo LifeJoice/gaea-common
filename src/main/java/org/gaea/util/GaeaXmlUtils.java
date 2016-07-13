b8321157c7106915c7f01973464ae84ab485f381
iverson <lifejoice@outlook.com>
2016-07-13 09:51:54 +0800

    修改copyAttributesToBean方法，捕捉并封装异常。不要把最底层的异常都往上抛。

--- src/main/java/org/gaea/util/GaeaXmlUtils.java
+++ src/main/java/org/gaea/util/GaeaXmlUtils.java
@@ -1,10 +1,12 @@
 package org.gaea.util;
 
 import org.apache.commons.lang3.ArrayUtils;
+import org.gaea.exception.InvalidDataException;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 
 import java.lang.reflect.InvocationTargetException;
+import java.text.MessageFormat;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,10 +14,10 @@ import java.util.Map;
  * Created by Iverson on 2015/7/6.
  */
 public class GaeaXmlUtils {
-    public static Map<String,String> getAttributes(Node node){
-        Map<String,String> attributes = new HashMap<String, String>();
+    public static Map<String, String> getAttributes(Node node) {
+        Map<String, String> attributes = new HashMap<String, String>();
         NamedNodeMap namedNodeMap = node.getAttributes();
-        if(namedNodeMap!=null) {
+        if (namedNodeMap != null) {
             for (int i = 0; i < namedNodeMap.getLength(); i++) {
                 Node n = namedNodeMap.item(i);
                 attributes.put(n.getNodeName(), n.getNodeValue());
@@ -24,7 +26,18 @@ public class GaeaXmlUtils {
         return attributes;
     }
 
-    public static <T>T copyAttributesToBean(Node orig, T destObj, Class<T> destClass) throws InvocationTargetException, IllegalAccessException {
+    /**
+     * 复制某xml元素的属性，到对应的bean（属性）中.<p/>
+     * 转换过程中,会自动去掉xml中元素属性的"-","_"分隔符，再和bean的属性名匹配再注入.
+     *
+     * @param orig
+     * @param destObj
+     * @param destClass
+     * @param <T>
+     * @return
+     * @throws InvalidDataException
+     */
+    public static <T> T copyAttributesToBean(Node orig, T destObj, Class<T> destClass) throws InvalidDataException {
         if (destObj == null) {
             return null;
         }
@@ -38,8 +51,14 @@ public class GaeaXmlUtils {
             // 清理XML中属性名中的下划线or中划线
             String cleanAttrName = GaeaStringUtils.removeHyphenAndUnderline(attrName);
             // 写入bean值的时候，因为属性名必须严格大小写一致，所以必须把xml属性名转为全小写后，去columnPropNames获取真正的严格大小写的bean属性名
-            if(ArrayUtils.contains(beanPropNamesArray,cleanAttrName.toLowerCase())) {      // 检查要写入的字段是否在目标对象存在，才可以写入
-                org.apache.commons.beanutils.BeanUtils.setProperty(destObj, beanPropNames.get(cleanAttrName.toLowerCase()), attributes.get(attrName));
+            if (ArrayUtils.contains(beanPropNamesArray, cleanAttrName.toLowerCase())) {      // 检查要写入的字段是否在目标对象存在，才可以写入
+                try {
+                    org.apache.commons.beanutils.BeanUtils.setProperty(destObj, beanPropNames.get(cleanAttrName.toLowerCase()), attributes.get(attrName));
+                } catch (IllegalAccessException e) {
+                    throw new InvalidDataException(MessageFormat.format("复制XML元素的属性到bean失败！元素名: {0} 元素内容: {1}", orig.getNodeName(), orig.getTextContent()).toString(), e, e.getMessage());
+                } catch (InvocationTargetException e) {
+                    throw new InvalidDataException(MessageFormat.format("复制XML元素的属性到bean失败！元素名: {0} 元素内容: {1}", orig.getNodeName(), orig.getTextContent()).toString(), e, e.getMessage());
+                }
             }
         }
         return destObj;
