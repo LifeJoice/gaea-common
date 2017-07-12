651dc286d307da6baf759fb35b8e63353ddc202a
iverson <lifejoice@outlook.com>
2017-07-12 10:39:33 +0800

    增加把json转List的通用工具方法。

--- src/main/java/org/gaea/util/GaeaJacksonUtils.java
+++ src/main/java/org/gaea/util/GaeaJacksonUtils.java
@@ -1,12 +1,16 @@
 package org.gaea.util;
 
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.gaea.exception.InvalidDataException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -23,5 +27,24 @@ public class GaeaJacksonUtils {
             return null;
         }
         return objectMapper.writeValueAsString(inObj);
+    }
+
+    /**
+     * 把一个字符串JSONString转换为一个{@code List<SomeClass>}
+     * @param jsonString       要被转换的json字符串
+     * @param implListClass    真实new的List类。例如：ArrayList
+     * @param genericClass     List中泛型对象T的Class。例如：DataItem.class
+     * @param <T>              List中泛型对象T的Class。例如：DataItem.class
+     * @return 例如{@code List<DateItem>}
+     * @throws InvalidDataException
+     */
+    public static <T> List<T> parseList(String jsonString, Class implListClass, Class<T> genericClass) throws InvalidDataException {
+        JavaType javaType = objectMapper.getTypeFactory().constructParametrizedType(implListClass, List.class, genericClass);
+        try {
+            List<T> parseList = objectMapper.readValue(jsonString, javaType);
+            return parseList;
+        } catch (IOException e) {
+            throw new InvalidDataException("转换json字符串为List<SomeClass>失败！json：" + jsonString, e);
+        }
     }
 }
